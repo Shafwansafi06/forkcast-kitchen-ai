a67c3ae19172c75c5157e0258aa0eeaa5cb1835b
Shafwansafi06 <Shafwansafi06@gmail.com>
2025-06-12 16:32:16 +0530

    Remove all PostHog code and restore main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { PostHogProvider } from 'posthog-js/react';
-import posthog from 'posthog-js';
 
-const options = {
-  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-};
-
-// Manual event for testing
-posthog.capture('my event', { property: 'value' });
-
-createRoot(document.getElementById("root")!).render(
-  <PostHogProvider
-    apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
-    options={options}
-  >
-    <App />
-  </PostHogProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
